41a4d9b5f331c72a1441ca9b610548efb187e085
masepe95 <marcosepe10@gmail.com>
2023-07-19 15:26:31 +0200

    added pswd logic

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
+$lenghtValue = $_GET['lenght-input'];
+var_dump($lenghtValue);
 
+
+function generatePassword($lenght)
+{
+    $specialArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '!', '=', '?', ':', '.', ';'];
+    $charArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'k', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+    $numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    $newPassword = [];
+
+    for ($i = 0; $i < $lenght; $i++) {
+
+        $charType = rand(1, 3);
+
+        if ($charType === 1) {
+
+            $takeSpecialchar = rand(0, 17);
+            array_push($newPassword, $specialArray[$takeSpecialchar]);
+        } elseif ($charType === 2) {
+
+            $takeChar = rand(0, 23);
+            array_push($newPassword, $charArray[$takeChar]);
+        } elseif ($charType === 3) {
+
+            $takeNumber = rand(0, 9);
+            array_push($newPassword, $numArray[$takeNumber]);
+        }
+    }
+
+    return implode($newPassword);
+};
+
+
+$newPassword = generatePassword($lenghtValue);
+
+var_dump($newPassword);
+
+?>
 
 
 ?>
